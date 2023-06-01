9e0012ff87ed49e6791fa1cac4bc44ce82484934
Sergey Shmatok <trycifer@gmail.com>
2023-06-01 15:14:56 +0300

    2

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { useFonts } from "expo-font";
+import RegistrationScreen from "./screens/RegistrationScreen/RegistrationScreen";
+import bgImage from "./assets/PhotoBG.png";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto: require("./assets/fonts/Roboto-Regular.otf"),
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js</Text>
+    <View
+      style={{
+        position: "absolute",
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+        // width: "100%",
+        height: 549,
+        left: 0,
+        top: 263,
+
+        // background: "#FFFFFF",
+        borderRadius: 25,
+      }}
+    >
+      <ImageBackground
+        source={bgImage}
+        resizeMode="cover"
+        style={styles.image}
+      ></ImageBackground>
+      <RegistrationScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+  },
+  image: {
+    flex: 1,
     justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   },
 });
